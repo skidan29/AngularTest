65c8ba20342f2a3935ba5816cc1b4240fa72696e
skidan29 <skidan2908@gmail.com>
2022-02-18 11:26:20 +0300

    Создание компонентов и сервисов

---
+++ src/app/premises-card/premises-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-premises-card',
+  templateUrl: './premises-card.component.html',
+  styleUrls: ['./premises-card.component.scss']
+})
+export class PremisesCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/premises-search/premises-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-premises-search',
+  templateUrl: './premises-search.component.html',
+  styleUrls: ['./premises-search.component.scss']
+})
+export class PremisesSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
