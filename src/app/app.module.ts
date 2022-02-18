b719d447dc85b7da8aba6eb5a45b253b8ecf2180
skidan29 <skidan2908@gmail.com>
2022-02-18 11:20:01 +0300

    create components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PremisesSearchComponent } from './premises-search/premises-search.component';
+import { PremisesCardComponent } from './premises-card/premises-card.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes =[
+  { path: '', component: PremisesSearchComponent },
+  { path: 'card/:id', component: PremisesCardComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremisesSearchComponent,
+    PremisesCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
