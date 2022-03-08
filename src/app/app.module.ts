0ed8d248720aa1e471be16d9520ad0a9e90b731c
skidan29 <skidan2908@gmail.com>
2022-03-08 22:15:32 +0300

    444

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PremisesSearchComponent } from './premises-search/premises-search.component';
 import { PremisesCardComponent } from './premises-card/premises-card.component';
-import {
-    RouterModule,
-    Routes,
-} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [
-    { path: '', component: PremisesSearchComponent },
-    { path: 'card', component: PremisesCardComponent },
+  { path: '', component: PremisesSearchComponent },
+  { path: 'card', component: PremisesCardComponent },
 ];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PremisesSearchComponent,
-        PremisesCardComponent,
-    ],
-    imports: [
-        ReactiveFormsModule,
-        BrowserModule,
-        RouterModule.forRoot(appRoutes),
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, PremisesSearchComponent, PremisesCardComponent],
+  imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/premises-search/premises-search.component.ts
+++ src/app/premises-search/premises-search.component.ts
@@ -1,92 +1,96 @@
-import {
-    Component,
-    OnInit,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PremisesSearchService } from './premises-search.service';
 import { Premises } from './premises-search.service';
-import {
-    FormControl,
-    FormGroup,
-} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
 
 interface FilterConfig {
-    garage: boolean;
-    office: boolean;
-    flat: boolean;
+  garage: boolean;
+  office: boolean;
+  flat: boolean;
 }
 
 @Component({
-    selector: 'app-premises-search',
-    templateUrl: './premises-search.component.html',
-    styleUrls: ['./premises-search.component.scss'],
-    providers: [PremisesSearchService],
+  selector: 'app-premises-search',
+  templateUrl: './premises-search.component.html',
+  styleUrls: ['./premises-search.component.scss'],
+  providers: [PremisesSearchService],
 })
-
 export class PremisesSearchComponent implements OnInit {
+  public premisesData: any;
+  public premises: any;
 
-    public premisesData: any;
-    public premises: any;
+  private defaultConfigFilter = {
+    garage: true,
+    office: true,
+    flat: true,
+  };
 
-    private defaultConfigFilter = {
-        garage: true,
-        office: true,
-        flat: true,
-    };
+  constructor(
+    private premisesSearchService: PremisesSearchService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
-    constructor(
-        private premisesSearchService: PremisesSearchService,
-        private router: Router,
-    ) {
-    }
-
-    ngOnInit(): void {
-        this.premisesSearchService.httpServer().subscribe(data => {
-            this.premisesData = data;
-            this.initPremises(this.defaultConfigFilter);
-        });
-    }
-
-    initPremises(type: FilterConfig) {
-        const premises = [];
-        if (type.garage) {
-            premises.push(...this.filterPremises('garage'));
-        }
-        if (type.office) {
-            premises.push(...this.filterPremises('office'));
-        }
-        if (type.flat) {
-            premises.push(...this.filterPremises('flat'));
-        }
-        this.premises = premises;
-    }
-
-    private filterPremises(type: string) {
-        return this.premisesData.filter((i: Premises) => i.type === type);
-    }
-
-    createObject() {
-        this.premises.push(this.premisesSearchService.createObject(this.premisesSearchService.setRandomNumber(2)));
-    }
-
-    public profileForm = new FormGroup({
-        flat: new FormControl(false),
-        office: new FormControl(false),
-        garage: new FormControl(false),
-
+  ngOnInit(): void {
+    this.premisesSearchService.httpServer().subscribe((data) => {
+      this.premisesData = data;
+      this.initPremises(this.defaultConfigFilter);
+      this.http
+        .get('http://localhost:3000/api/garages')
+        .subscribe((data) => console.log(data));
     });
+  }
 
-    onChangeForm() {
-        const formValidation = Object.values(this.profileForm.value).find(value => value === true);
-        if (formValidation) {
-            this.initPremises(this.profileForm.value);
-        } else {
-            this.initPremises(this.defaultConfigFilter);
-        }
+  initPremises(type: FilterConfig) {
+    const premises = [];
+    if (type.garage) {
+      premises.push(...this.filterPremises('garage'));
     }
+    if (type.office) {
+      premises.push(...this.filterPremises('office'));
+    }
+    if (type.flat) {
+      premises.push(...this.filterPremises('flat'));
+    }
+    this.premises = premises;
+  }
 
-    public onTheRouteCard(premise: Premises) {
-        this.router.navigate(['card'], { queryParams: premise });
+  private filterPremises(type: string) {
+    return this.premisesData.filter((i: Premises) => i.type === type);
+  }
+
+  createObject() {
+    this.http
+      .post('http://localhost:3000/api/garages', { name: 12121 })
+      .subscribe((data) => console.log(data));
+
+    this.premises.push(
+      this.premisesSearchService.createObject(
+        this.premisesSearchService.setRandomNumber(2)
+      )
+    );
+  }
+
+  public profileForm = new FormGroup({
+    flat: new FormControl(false),
+    office: new FormControl(false),
+    garage: new FormControl(false),
+  });
+
+  onChangeForm() {
+    const formValidation = Object.values(this.profileForm.value).find(
+      (value) => value === true
+    );
+    if (formValidation) {
+      this.initPremises(this.profileForm.value);
+    } else {
+      this.initPremises(this.defaultConfigFilter);
     }
+  }
+
+  public onTheRouteCard(premise: Premises) {
+    this.router.navigate(['card'], { queryParams: premise });
+  }
 }
