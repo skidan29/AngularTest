ced6b3736cfee42206101655dea5c712786e30ae
skidan29 <63918439+skidan29@users.noreply.github.com>
2022-05-20 17:06:27 +0300

    Вычестил компонент

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PremisesSearchComponent } from './premises-search/premises-search.component';
-import { PremisesCardComponent } from './premises-card/premises-card.component';
-import { RouterModule, Routes } from '@angular/router';
+import {
+    RouterModule,
+    Routes,
+} from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { FilterButtonsComponent } from './premises-search/filter-buttons/filter-buttons.component';
+
 
 const appRoutes: Routes = [
-  { path: '', component: PremisesSearchComponent },
-  { path: 'card/:id', component: PremisesCardComponent },
+    { path: '', component: AppComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, PremisesSearchComponent, PremisesCardComponent, FilterButtonsComponent],
-  imports: [
-    HttpClientModule,
-    ReactiveFormsModule,
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
